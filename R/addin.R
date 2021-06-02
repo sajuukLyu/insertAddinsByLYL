dba7619d74849b88a9582d94f08de17b18196e42
QTC.Yulin <lvyulin@pku.edu.cn>
2021-06-02 18:21:59 +0800

    <feat> add a new addin

--- R/addin.R
+++ R/addin.R
@@ -1,6 +1,20 @@
 
+#' Insert \%<>\%
+#'
+#' Call this function as an addin to insert \code{ \%<>\% } at the cursor position.
+#'
+#' @export
 insertLeftPipeAddin <- function() {
   rstudioapi::insertText(" %<>% ")
+}
+
+#' Insert function template
+#'
+#' Call this function as an addin to insert \code{ function(x) \{x\} } at the cursor position.
+#'
+#' @export
+insertFuncTempAddin <- function() {
+  rstudioapi::insertText("function(x) {x}")
 }
 
 
